93ff5f64869f7101c28c7bd08561e331d93c2f06
andyka adi <andykaadi23@gmail.com>
2023-09-01 23:05:59 +0700

    prepare new items

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const newItem = { name, quantity, checked: false, id: Date.now() };
+
+    console.log(newItem);
+
+    setName("");
+    setQuantity("");
+  };
+
   const quantityNum = [...Array(10)].map((x, i) => (
     <option value={i + 1} key={i + 1}>
       {i + 1}
@@ -6,10 +24,20 @@ const Form = () => {
   ));
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <div>
-        <select>{quantityNum}</select>
-        <input type="text" placeholder="nama barang..." />
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {quantityNum}
+        </select>
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <button>Tambah</button>
       </div>
     </form>
